28520b86633929fe9174eca13f4ffeac73ea035c
shuvo11 <shuvoas64@gmail.com>
2023-07-09 10:02:33 +0600

    first commit

---
+++ resources/views/panels/style.blade.php
@@ -0,0 +1,25 @@
+ <!-- BEGIN: Vendor CSS-->
+ <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/vendors/css/vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/vendors/css/charts/apexcharts.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/vendors/css/extensions/toastr.min.css')}}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/themes/bordered-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/themes/semi-dark-layout.css')}}">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/core/menu/menu-types/vertical-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/pages/dashboard-ecommerce.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/plugins/charts/chart-apex.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/plugins/extensions/ext-component-toastr.css')}}">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::to('/app-assets/css/plugins/extensions/ext-component-toastr.css')}}">
+    <!-- END: Custom CSS-->
